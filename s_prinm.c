78a80384227933b71783bf182dfb84a85fda8953
pretsolomi <pretsolomi@gmail.com>
2026-05-12 00:03:55 +0530

    Create s_prinm.c

---
+++ s_prinm.c
@@ -0,0 +1,70 @@
+// Prim's Algorithm - Minimum Spanning Tree
+
+#include <stdio.h>
+
+#define MAX 10
+#define INF 999
+
+int main()
+{
+    int cost[MAX][MAX];
+    int visited[MAX] = {0};
+
+    int n, i, j;
+    int min, mincost = 0;
+    int a, b;
+
+    printf("Enter number of vertices: ");
+    scanf("%d", &n);
+
+    printf("Enter cost matrix:\n");
+
+    // Input matrix
+    for(i = 0; i < n; i++)
+    {
+        for(j = 0; j < n; j++)
+        {
+            scanf("%d", &cost[i][j]);
+
+            if(cost[i][j] == 0)
+                cost[i][j] = INF;
+        }
+    }
+
+    visited[0] = 1;
+
+    printf("\nEdges in MST:\n");
+
+    // n-1 edges required
+    for(int edge = 0; edge < n - 1; edge++)
+    {
+        min = INF;
+
+        // Find minimum edge
+        for(i = 0; i < n; i++)
+        {
+            if(visited[i] == 1)
+            {
+                for(j = 0; j < n; j++)
+                {
+                    if(visited[j] == 0 && cost[i][j] < min)
+                    {
+                        min = cost[i][j];
+                        a = i;
+                        b = j;
+                    }
+                }
+            }
+        }
+
+        printf("%d -> %d = %d\n", a, b, min);
+
+        mincost += min;
+
+        visited[b] = 1;
+    }
+
+    printf("\nMinimum Cost = %d", mincost);
+
+    return 0;
+}
